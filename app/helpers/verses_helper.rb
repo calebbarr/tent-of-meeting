855e8bbcaa66353d5bd41d772f96b36c659c61f4
Caleb Barr <caleb@calebbarr.com>
2012-02-20 22:58:55 -0500

    embed mp3 player for verses

--- app/helpers/verses_helper.rb
+++ app/helpers/verses_helper.rb
@@ -1,2 +1,16 @@
 module VersesHelper
+  def audio_url(verse_name,chapter_name,book_name,book_id_string)
+    book_stem = VERSE_AUDIO_BOOK_ABBREV.has_key?(book_name) ? VERSE_AUDIO_BOOK_ABBREV[book_name] : book_name.gsub(/ /,"")[0,3].upcase
+    if book_id_string.length < 2 then
+      book_id_string = "0"+book_id_string
+    end
+    if chapter_name.length < 2 then
+      chapter_name = "0"+chapter_name
+    end
+    if verse_name.length < 2 then
+      verse_name = "0"+verse_name
+    end
+    book_prefix = book_id_string + "_" + book_stem
+    return VERSE_AUDIO_URL_STEM + "/" + book_prefix + "/" + book_prefix + "_" + chapter_name + "_" + verse_name +".MP3"
+  end
 end
--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,3 +3,10 @@ NUMBER_OF_CHAPTERS = 1189
 NUMBER_OF_BOOKS = 66
 LAST_VERSE_NAME = 21
 LAST_CHAPTER_NAME = 22
+VERSE_AUDIO_URL_STEM = "http://www.ebibleaudio.com/Radio/MP3%2032800%20Single%20Verse%20Files"
+VERSE_AUDIO_BOOK_ABBREV ={
+  "Philippians" => "PHP",
+  "Philemon" => "PHM",
+  "Jude" => "JDE"
+  
+}
