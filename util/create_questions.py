f2c5e036b7907be6cdb47176f0b75b1a0b3f7de5
Caleb Barr <caleb@calebbarr.com>
2012-02-23 01:36:19 -0500

    fixed various scripts

---
+++ util/create_questions.py
@@ -0,0 +1,110 @@
+# script to read in verses and prompt user to create questions from command line
+# this can be done through the web UI now
+# loads from verses_path below, all uncommented verses must be processed in order to produce output
+# a signal handler should be implemented to print output when the program is killed, instead
+
+verses_path = "../resources/verses_data"
+
+verses_lines = open(verses_path).readlines()
+
+
+books = {}
+
+currbook = ""
+currchap = ""
+currverse = ""
+
+index = 0
+while index < len(verses_lines):
+	line = verses_lines[index]
+	line_array = line.split()
+	if len(line_array) == 1:
+		currbook = line_array[0]
+		index+=1
+		line = verses_lines[index]
+		line_array = line.split()		
+		currchap = line_array[0]
+		if currbook not in books:
+			books[currbook] = {}
+		if currchap not in books[currbook]:
+			books[currbook][currchap] = {}
+		
+		index+=1
+		
+	line = verses_lines[index]
+	line_array = line.split()
+	versenum = line_array[0]
+	if versenum == "#":
+		index +=1
+	else:
+		if versenum not in books[currbook][currchap]:
+			books[currbook][currchap][versenum] = {}
+		rest_of_verse = " ".join(line_array[1:len(line_array)])
+	
+		print "\n\nPlease supply a question for the following verse. Questions are assumed to be within the context of their verse.\n\n"
+		next_question = raw_input(rest_of_verse+"\n\n\n")
+		while next_question.strip("\n") != "":
+			books[currbook][currchap][versenum][next_question.strip("\n")] = { "choices" : []}
+		
+			counter = 1
+			print "please give 4 multiple choice answers for the question"
+			while counter < 5:
+				books[currbook][currchap][versenum][next_question.strip("\n")]["choices"].append(raw_input("answer "+str(counter)+":"))
+				counter +=1
+		
+			correct = raw_input("which was the correct answer?\n").strip("\n")
+			while(correct != "1" and correct != "2" and correct != "3" and correct != "4"):
+				print "please input a number 1-4:"
+				correct = raw_input("which was the correct answer?\n").strip("\n")
+			books[currbook][currchap][versenum][next_question.strip("\n")]["correct"] = correct
+			print "Please supply a question for the following verse.  If you want to move on, simply press enter.\n\n"
+			next_question = raw_input(rest_of_verse+"\n\n\n")
+		index+=1
+		
+ 
+
+quiz = raw_input("These questions are about to be written to a file.  Do you want to take the quiz now? Type 'yes' if so.\n") == "yes"
+
+quiz_file_path = "./quiz_file"
+output_file = open(quiz_file_path,'a')
+
+
+bible_books = books.keys()
+for book in bible_books:
+	book_chapters= books[book].keys()
+	for chapter in book_chapters:
+		chapter_verses = books[book][chapter].keys()
+		for verse in chapter_verses:
+			reference = book + " "+chapter+":"+verse
+			questions = books[book][chapter][verse].keys()
+			for question in questions:
+				answers = books[book][chapter][verse][question]["correct"]
+				prompt = "According to " + reference+", "+question
+				if question[-1] != '?':
+					prompt +="?"
+					
+				if quiz:
+					print prompt
+					
+				output_file.write(prompt+"\n")
+				counter = 1
+				
+				correct = books[book][chapter][verse][question]["correct"]
+				for choice in books[book][chapter][verse][question]["choices"]:
+					if quiz:
+						print str(counter)+":\t"+choice
+					if str(counter) == correct:
+						output_file.write("*")
+					output_file.write(choice+"\n")
+					counter+=1
+				output_file.write("\n")	
+				if quiz:
+					answer = raw_input("Answer: ")
+
+				
+				if quiz:
+					if answer.strip("\n") != correct:
+						print "Sorry, the answer was " + correct
+					else:
+						print "Correct"
+			output_file.write("\n")
---
+++ util/seed_from_quiz_fragment.py
@@ -0,0 +1,71 @@
+#script to convert output of python generation script to rails seeding script
+
+# VerseText.create({id: 31102,verse_id: 31102, language: "eng", content: "The grace of our Lord Jesus Christ be with you all. Amen."})
+#<MultipleChoiceQuestion id: 1, verse_id: 1, content: "According to Genesis 1:1, when did God create the h...", a: "after the earth", b: "before the earth", c: "in the beginning", d: "ages ago", correct: "c", created_at: "2012-02-19 07:06:09", updated_at: "2012-02-19 07:06:09"> 
+import re,os,sys
+
+new_seeds = open("../resources/generated_seeds.rb","a")
+numbers_letters = {
+1 : "a",
+2 : "b",
+3 : "c",
+4 : "d"
+}
+
+question_answers = open('../resources/quiz_file','r').read().split('\n\n')
+
+for question_answer in question_answers:
+	output ="MultipleChoiceQuestion.create({"
+	lines = question_answer.split("\n")
+	if len(lines) != 5:
+		print lines
+		print "len(lines) != 5:"
+		sys.exit()
+	question = lines[0]
+	tokens = question.split()
+	if tokens[0] != "According" or tokens[1] != "to" or tokens[2] != tokens[2].capitalize() or not re.match("[0-9]+:[0-9]", tokens[3]):
+		print lines
+		print "bad input"
+		sys.exit()
+	book = tokens[2]
+	chapter,verse = tokens[3].split(":")
+	verse = verse[:-1]
+	print "doing ruby lookup"
+	print book,chapter,verse
+	verse_id = os.popen("ruby get_verse_id.rb "+ book +" "+ chapter+ " "+verse).read()
+	
+	correct = None
+	for index in range(1,5):
+		if lines[index][0] == "*":
+			lines[index] = lines[index][1:]
+			correct = numbers_letters[index]
+			
+	if correct == None:
+		print lines
+		print "correct == None:"
+		sys.exit()
+	
+	output+=" verse_id: "
+	output += str(verse_id)
+	output+=", "
+	output+=" content: \""
+	output += 	question
+	output+="\", "
+	output+=" a: \""
+	output += lines[1]
+	output+="\", "
+	output+=" b: \""
+	output += lines[2]
+	output+="\", "
+	output+=" c: \""
+	output += lines[3]
+	output+="\", "
+	output+=" d: \""	
+	output += lines[4]
+	output+="\", "
+	output+=" correct: \""
+	output += correct
+	output+=" })"
+	new_seeds.write(output+"\n")
+	
+new_seeds.close()
