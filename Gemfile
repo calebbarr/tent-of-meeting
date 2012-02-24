adaee13db3bbdeed7464806ac9605f2f0b860efd
Caleb Barr <caleb@calebbarr.com>
2012-02-23 22:16:21 -0500

    removed client side validations, had some problems with asset precompiling in heroku

--- Gemfile
+++ Gemfile
@@ -11,7 +11,7 @@ gem 'haml-rails'
 gem 'devise'
 gem 'heroku'
 gem 'kaminari'
-gem 'client_side_validations'
+
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ resources/generated_questions.rb
@@ -0,0 +1,2 @@
+MultipleChoiceQuestion.create({ verse_id: 17025 , content: "According to Proverbs 22:9, what descriptor is applied to the eye of the person who will be blessed?", a: "good", b: "bountiful", c: "true", d: "discerning", correct: "b" })
+MultipleChoiceQuestion.create({ verse_id: 17025 , content: "According to Proverbs 22:9, why shall the person with a bountiful eye be blessed?", a: "he directeth his work in truth", b: "he raiseth up his children after him", c: "he discerneth to answer", d: "he giveth of his bread to the poor", correct: "d" })
